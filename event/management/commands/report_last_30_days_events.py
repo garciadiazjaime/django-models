ef3962601e01469f78fbbb3118eca5d61fe26cfc
Jaime <garciadiazjaime@gmail.com>
2024-06-09 11:19:39 -0500

    feat: report content type

--- event/management/commands/report_last_30_days_events.py
+++ event/management/commands/report_last_30_days_events.py
@@ -1,11 +1,13 @@
 from datetime import datetime, timedelta, date
-
-from django.core.management.base import BaseCommand
-from event.models import Event
 from pathlib import Path
 import boto3
 import json
+
+
+from django.core.management.base import BaseCommand
 from django.core import serializers
+
+from event.models import Event
 
 
 class Command(BaseCommand):
@@ -17,11 +19,15 @@ class Command(BaseCommand):
             created__gt=datetime.now() - timedelta(days=30)
         ).order_by("created")
 
+        created = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
         data = serializers.serialize("json", events)
+        output = {"created": created, "data": json.loads(data)}
 
         s3 = boto3.resource("s3")
         s3.Bucket("cmc.data").put_object(
-            Key="report_last_30_days_events.json", Body=json.dumps(data)
+            Key="data/report_last_30_days_events.json",
+            Body=bytes(json.dumps(output).encode("UTF-8")),
+            ContentType="application/json",
         )
         print(f"{events.count()} events exported")
         print("export completed")
